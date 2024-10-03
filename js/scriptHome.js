4d1896bd07bdbdc02b71d5979c87b802af996224
Aldana Vega <aldanavega14@gmail.com>
2024-10-03 00:57:00 -0300

    .

---
+++ js/scriptHome.js
@@ -0,0 +1,64 @@
+import {peliculasPopulares,romance,terror,accion,animacion,recomendados} from "../js/informacion/peliculas.js";
+import {seriesPopulares} from "../js/informacion/series.js"
+
+function mostrar(items, nodo, aMostrar) {
+    const fragment = document.createDocumentFragment();
+    switch (aMostrar) {
+        case 'poster':
+            items.forEach((item) => {      
+                let link = document.createElement('a');
+                link.href = `info.html?titulo=${item.titulo}&tipo=${item.tipo}`; // URL de la página de detalles
+            
+                let img = document.createElement('img');
+                img.src = item.poster;
+                img.alt = "Poster pelicula";
+
+                link.appendChild(img);
+                fragment.appendChild(link);
+            });
+            break;
+        case 'descripcion':
+            items.forEach((item) => {
+                let p = document.createElement('p');
+                p.innerHTML = item.sinopsis;
+                fragment.appendChild(p);
+            });
+            break;
+    }
+
+    console.log(fragment);
+    nodo.appendChild(fragment);
+}
+
+
+/*Busco nodo de populares y ubico las imagenes*/
+let popularNodo = document.getElementById('peliculas_popular')
+console.log(popularNodo);
+mostrar(peliculasPopulares, popularNodo, 'poster');
+
+let popularNodo2 = document.getElementById('series_popular')
+console.log(popularNodo);
+mostrar(seriesPopulares, popularNodo2, 'poster');
+
+/*Busco nodo de recomendadas y ubico las imagenes*/
+let recomendadoNodo = document.getElementById('recomendado')
+mostrar(recomendados, recomendadoNodo, 'poster');
+
+/*Busco nodo de accion y ubico las imagenes*/
+let accionNodo = document.getElementById('accion')
+mostrar(accion, accionNodo, 'poster');
+
+/*Busco nodo de accion y ubico las imagenes*/
+let terrorNodo = document.getElementById('terror')
+mostrar(terror, terrorNodo, 'poster');
+
+/*Busco nodo de terror y ubico las imagenes*/
+let animacionNodo = document.getElementById('animacion')
+mostrar(animacion, animacionNodo, 'poster');
+
+/*Busco nodo de terror y ubico las imagenes*/
+let romanceNodo = document.getElementById('romance')
+mostrar(romance, romanceNodo, 'poster');
+
+
+
---
+++ js/scriptNavBar.js
@@ -0,0 +1,70 @@
+let navBar = document.getElementById('navBar');
+
+const ul = document.createElement('ul');
+
+//LOGO
+const logo = document.createElement('li');
+logo.className = 'logo';
+
+const a = document.createElement('a');
+a.href = "../../home.html";
+const logoImg = document.createElement('img');
+logoImg.className = 'logo';
+logoImg.src = "../imagenes/logo1.png";
+
+a.appendChild(logoImg);
+logo.appendChild(a);
+ul.appendChild(logo);
+
+
+//BARRA DE BUSQUEDA
+const barra = document.createElement('li');
+barra.className = "search-icon";
+const input = document.createElement('input');
+input.type = "search";
+input.placeholder = "Buscar";
+const lupa = document.createElement('i');
+lupa.className = 'fa fa-search'; // Agregar las clases necesarias para el icono
+lupa.setAttribute('aria-hidden', 'true');
+
+barra.appendChild(input);
+barra.appendChild(lupa);
+ul.appendChild(barra);
+
+
+//BOTON
+const boton = document.createElement('li');
+boton.className = "boton";
+boton.id= "boton";
+const icono = document.createElement('i');
+icono.className = 'fa fa-bars'; // Agregar las clases necesarias para el icono
+icono.setAttribute('aria-hidden', 'true');
+boton.appendChild(icono);
+ul.appendChild(boton);
+
+
+//ITEMS
+const items = document.createElement('div');
+items.className = "items";
+items.id = "items";
+
+const itemsLista = ["Listas", "Watchlist", "Perfil"];
+itemsLista.forEach(item => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = "#";
+    a.textContent = item;
+    li.appendChild(a);
+    items.appendChild(li);
+});
+ul.appendChild(items);
+
+navBar.appendChild(ul);
+
+
+
+const b = document.getElementById('boton');
+const i = document.getElementById('items');
+b.addEventListener('click',()=>{
+      i.classList.toggle('show');
+    });
